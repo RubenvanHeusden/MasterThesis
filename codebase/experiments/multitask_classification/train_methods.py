fixed a bug in the f1 score calculation

codebase/experiments/multitask_classification/train_methods.py
@@ -8,6 +8,7 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from itertools import cycle, repeat
+from collections import defaultdict
 
 
 # TODO: towers should be a dict of shape "{"example_task_tower": tower}"
@@ -25,14 +26,10 @@ def train(model, criterion, optimizer, scheduler, dataset, n_epochs=5, device=to
             with open("%s/model_params.txt" % save_path, "w") as f_obj:
                 f_obj.write(str(model.params))
             shutil.copyfile("config.py", "%s/config.py" % save_path)
-
+        all_predictions = defaultdict(list)
+        all_ground_truth_labels = defaultdict(list)
+        epoch_running_loss = defaultdict(float)
         # Calculate several training statistics
-        epoch_running_loss = 0.0
-        epoch_precision = 0
-        epoch_recall = 0
-        epoch_correct = 0
-        epoch_total = 0
-        epoch_f1 = 0
         for i, batch in tqdm(enumerate(dataset)):
             batch_running_loss = 0.0
             optimizer.zero_grad()
@@ -40,33 +37,27 @@ def train(model, criterion, optimizer, scheduler, dataset, n_epochs=5, device=to
             outputs = model(X, task)
 
             loss = criterion(outputs, y)
-            batch_running_loss += loss.item()
-
-            # Calculating several batch statistics
-            batch_correct = (torch.max(outputs, 1)[1].view(y.size()).data == y.data).sum().item()
-            batch_f1 = f1_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-            batch_recall = recall_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-            batch_precision = precision_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-            epoch_recall += batch_recall
-            epoch_precision += batch_precision
-            epoch_f1+=batch_f1
-            epoch_running_loss += batch_running_loss
-            epoch_correct += batch_correct
-            epoch_total += outputs.shape[0]
             # training the network
             loss.backward()
             optimizer.step()
-            # print("[Epoch: %d, Batch: %d, Loss: %.3f, Acc: %.3f]" % (epoch + 1, i+1, batch_running_loss,
-            #                                                          batch_correct / batch_total))
-            # print statistics
-            # print every 2000 mini-batches
-        scheduler.step()
-        prog_string = "[|Train| Loss: %.3f, Acc: %.3f, f_1: %.3f, recall: %.3f, precision, %.3f]" \
-                      % (epoch_running_loss, epoch_correct / epoch_total,
-                         epoch_f1 / (i+1), epoch_recall / (i+1), epoch_precision / (i+1))
-        with open("%s/results.txt" % save_path, "a") as f:
-            f.write(prog_string+"\n")
+            all_predictions[task].extend(outputs.detach().cpu().argmax(1).tolist())
+            all_ground_truth_labels[task].extend(y.cpu().tolist())
+            epoch_running_loss[task] += loss.item()
+            # Calculating several batch statistics
 
+        scheduler.step()
+        for task in all_predictions.keys():
+            correct_list = [1 if a == b else 0 for a, b in zip(all_predictions[task], all_ground_truth_labels[task])]
+            acc = sum(correct_list) / len(correct_list)
+            prog_string = "[|Train| Task: %s Loss: %.3f, Acc: %.3f, f_1: %.3f, recall: %.3f, precision, %.3f]" \
+                          % (task, epoch_running_loss[task], acc,
+                     f1_score(all_ground_truth_labels[task], all_predictions[task], average="micro"),
+                     recall_score(all_ground_truth_labels[task], all_predictions[task], average="micro"),
+                     precision_score(all_ground_truth_labels[task], all_predictions[task], average="micro"))
+            with open("%s/results.txt" % save_path, "a") as f:
+                f.write(prog_string+"\n")
+        with open("%s/results.txt" % save_path, "a") as f:
+            f.write("\n")
     print('Finished Training')
     torch.save(model.state_dict(), "%s/%s_epoch_%d.pt" % (save_path, save_name, epoch))
     #print(model.softmax(model.gating_network(inputs, lengths=lengths)).unsqueeze(1))
@@ -78,37 +69,25 @@ def evaluation(model, dataset, criterion, device=None):
     # Set the model to evaluation mode, important because of the Dropout Layers
     model = model.eval()
     # Calculate several test statistics
-    epoch_running_loss = 0.0
-    epoch_correct = 0
-    epoch_total = 0
-    epoch_f1 = 0
-    epoch_precision = 0
-    epoch_recall = 0
-    for i, batch in tqdm(enumerate(dataset)):
+    epoch_running_loss = defaultdict(float)
+    all_predictions = defaultdict(list)
+    all_ground_truth_labels = defaultdict(list)
+    for i, batch in enumerate(dataset):
         X, y, task = batch
-        if isinstance(X, tuple):
-            X = list(X)
-            for k in range(len(X)):
-                X[k] = X[k].to(device)
-        else:
-            X = X.to(device)
         outputs = model(X, task)
 
         loss = criterion(outputs, y)
-        epoch_running_loss += loss.item()
-        # Calculate several batch statistics
-        batch_correct = (torch.max(outputs, 1)[1].view(y.size()).data == y.data).sum().item()
-        batch_f1 = f1_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-        batch_recall = recall_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-        batch_precision = precision_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-        epoch_recall += batch_recall
-        epoch_precision += batch_precision
-        epoch_correct += batch_correct
-        epoch_f1 += batch_f1
-        epoch_total += outputs.shape[0]
-
-    prog_string = "[|Test| Loss: %.3f, Acc: %.3f, f_1: %.3f, recall: %.3f, precision, %.3f]" \
-                  %(epoch_running_loss, epoch_correct/epoch_total,
-                                                           epoch_f1/(i+1), epoch_recall/(i+1), epoch_precision/(i+1))
-    print(prog_string)
+        epoch_running_loss[task] += loss.item()
+        all_predictions[task].extend(outputs.detach().cpu().argmax(1).tolist())
+        all_ground_truth_labels[task].extend(y.cpu().tolist())
+       # Calculate several batch statistics
+    for task in all_predictions.keys():
+        correct_list = [1 if a == b else 0 for a, b in zip(all_predictions[task], all_ground_truth_labels[task])]
+        acc = sum(correct_list) / len(correct_list)
+        prog_string = "[|Train| Task: %s Loss: %.3f, Acc: %.3f, f_1: %.3f, recall: %.3f, precision, %.3f]" \
+                      % (task, epoch_running_loss[task], acc,
+                 f1_score(all_ground_truth_labels[task], all_predictions[task], average="micro"),
+                 recall_score(all_ground_truth_labels[task], all_predictions[task], average="micro"),
+                 precision_score(all_ground_truth_labels[task], all_predictions[task], average="micro"))
+        print(prog_string)
 

codebase/experiments/single_task_classification/train_methods.py
@@ -22,43 +22,35 @@ def train(model, criterion, optimizer, scheduler, dataset, n_epochs=5, device=to
             shutil.copyfile("config.py", "%s/config.py" % save_path)
 
         # Calculate several training statistics
+        all_predictions = []
+        all_ground_truth_labels = []
         epoch_running_loss = 0.0
-        epoch_precision = 0
-        epoch_recall = 0
-        epoch_correct = 0
-        epoch_total = 0
-        epoch_f1 = 0
-        epoch_acc = 0
 
         for i, batch in tqdm(enumerate(dataset)):
-            batch_running_loss = 0.0
             optimizer.zero_grad()
             X, y, _ = batch
             # Whether the padding should be removed when fed into the LSTM
             outputs = model(X)
 
             loss = criterion(outputs, y)
-            batch_running_loss += loss.item()
-
-            # Calculating several batch statistics
-            batch_correct = (torch.max(outputs, 1)[1].view(y.size()).data == y.data).sum().item()
-            batch_f1 = f1_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-            batch_recall = recall_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-            batch_precision = precision_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-            batch_acc = accuracy_score(y.cpu(), outputs.detach().cpu().argmax(1))
-            epoch_acc += batch_acc
-            epoch_recall += batch_recall
-            epoch_precision += batch_precision
-            epoch_f1 += batch_f1
-            epoch_running_loss += batch_running_loss
-            epoch_correct += batch_correct
-            epoch_total += outputs.shape[0]
             # training the network
             loss.backward()
             optimizer.step()
+
+            # Calculating several batch statistics
+
+            all_predictions.extend(outputs.detach().cpu().argmax(1).tolist())
+            all_ground_truth_labels.extend(y.cpu().tolist())
+            epoch_running_loss += loss.item()
+
+        correct_list = [1 if a == b else 0 for a, b in zip(all_predictions, all_ground_truth_labels)]
+        acc = sum(correct_list) / len(correct_list)
         prog_string = "[|Train| Loss: %.3f, Acc: %.3f, f_1: %.3f, recall: %.3f, precision, %.3f]" \
-                      % (epoch_running_loss, epoch_correct / epoch_total,
-                         epoch_f1 / (i+1), epoch_recall / (i+1), epoch_precision / (i+1))
+                      % (epoch_running_loss, acc,
+                         f1_score(all_ground_truth_labels, all_predictions, average="micro"),
+                         recall_score(all_ground_truth_labels, all_predictions, average="micro"),
+                         precision_score(all_ground_truth_labels, all_predictions, average="micro"))
+
         with open("%s/results.txt" % save_path, "a") as f:
             f.write(prog_string+"\n")
 
@@ -68,27 +60,14 @@ def train(model, criterion, optimizer, scheduler, dataset, n_epochs=5, device=to
     return model
 
 
-
-
-
-
-
-
-
-
-
-
 def evaluation(model, dataset, criterion, include_lengths=True, device=None):
     model.to(device)
     # Set the model to evaluation mode, important because of the Dropout Layers
     model = model.eval()
     # Calculate several test statistics
     epoch_running_loss = 0.0
-    epoch_correct = 0
-    epoch_total = 0
-    epoch_f1 = 0
-    epoch_precision = 0
-    epoch_recall = 0
+    all_predictions = []
+    all_ground_truth_labels = []
     for i, batch in tqdm(enumerate(dataset)):
         X, y, _ = batch
         outputs = model(X)
@@ -96,19 +75,17 @@ def evaluation(model, dataset, criterion, include_lengths=True, device=None):
         loss = criterion(outputs, y)
         epoch_running_loss += loss.item()
         # Calculate several batch statistics
-        batch_correct = (torch.max(outputs, 1)[1].view(y.size()).data == y.data).sum().item()
-        batch_f1 = f1_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-        batch_recall = recall_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-        batch_precision = precision_score(y.cpu(), outputs.detach().cpu().argmax(1), average='micro')
-        epoch_recall += batch_recall
-        epoch_precision+=batch_precision
-        epoch_correct += batch_correct
-        epoch_f1 += batch_f1
-        epoch_total += outputs.shape[0]
+        all_predictions.extend(outputs.detach().cpu().argmax(1).tolist())
+        all_ground_truth_labels.extend(y.cpu().tolist())
 
-    prog_string = "[|Test| Loss: %.3f, Acc: %.3f, f_1: %.3f, recall: %.3f, precision, %.3f]" \
-                  %(epoch_running_loss, epoch_correct/epoch_total,
-                                                           epoch_f1/(i+1), epoch_recall/(i+1), epoch_precision/(i+1))
+    correct_list = [1 if a == b else 0 for a, b in zip(all_predictions, all_ground_truth_labels)]
+    acc = sum(correct_list) / len(correct_list)
+    prog_string = "[|Train| Loss: %.3f, Acc: %.3f, f_1: %.3f, recall: %.3f, precision, %.3f]" \
+                  % (epoch_running_loss, acc,
+                     f1_score(all_ground_truth_labels, all_predictions, average="micro"),
+                     recall_score(all_ground_truth_labels, all_predictions, average="micro"),
+                     precision_score(all_ground_truth_labels, all_predictions, average="micro"))
+
     print(prog_string)
 
 